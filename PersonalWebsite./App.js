4c42311a96f3e9bdb630444d3d9dd280197a9653
Sparsh Patel <48163939+Sparsh101AI@users.noreply.github.com>
2023-06-29 18:01:12 -0400

    Add files via upload

---
+++ PersonalWebsite./App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from "./components/NavBar";
+import { Banner } from "./components/Banner";
+import { Skills } from "./components/Skills";
+import { Projects } from "./components/Projects";
+import { Myjourney } from "./components/Myjourney";
+import { Footer } from "./components/Footer";
+import { Aboutme } from "./components/Aboutme";
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <Aboutme/>
+      <Skills />
+      <Projects />
+      <Myjourney />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
